2de0bdf6ad4b5a8154c9f55e09e9f217de834501
ciminoce <ciminoce@yahoo.com.ar>
2022-09-08 16:39:24 -0300

    Agregar archivos de proyecto.

---
+++ LocosPOO.Consola/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using LocosPOO.Entidades;
+
+namespace LocosPOO.Consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello Crazy Numbers!");
+                var locos = new NumerosLocos();
+                Console.WriteLine(locos.MostrarDatos(locos.GetNumeros));
+                Console.WriteLine(locos.MostrarPositivosDecreciente());
+                Console.WriteLine(locos.MostrarNegativosCreciente());
+                var numero = locos[74];
+                Console.WriteLine(numero);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                ;
+            }            Console.ReadLine();
+        }
+    }
+}
---
+++ LocosPOO.Entidades/NumerosLocos.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace LocosPOO.Entidades
+{
+    public class NumerosLocos
+    {
+        private int[] numeros;
+        public int[] GetNumeros => numeros;
+        public NumerosLocos():this(20)
+        {
+            
+        }
+
+        public NumerosLocos(int capacidad)
+        {
+            if (capacidad<20)
+            {
+                throw new ArgumentException("Capacidad menor a la permitida: 20");
+            }
+
+            numeros = new int[capacidad];
+            PopularNumeros();
+        }
+
+        private void PopularNumeros()
+        {
+            Random r = new Random();
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                numeros[i] = r.Next(-20, 20);
+            }
+        }
+
+        public string MostrarDatos(int[] nros)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var nro in nros)
+            {
+                sb.Append($"{nro} ");
+            }
+
+            return sb.ToString();
+        }
+
+        public string MostrarPositivosDecreciente()
+        {
+            var positivos = numeros.Where(n => n > 0)
+                .OrderByDescending(n => n)
+                .ToArray();
+            return this.MostrarDatos(positivos);
+        }
+
+        public string MostrarNegativosCreciente()
+        {
+            var negativos = numeros.Where(n => n < 0)
+                .OrderBy(n => n).ToArray();
+            return this.MostrarDatos(negativos);
+        }
+
+        public int GetDato(int index)
+        {
+            return numeros[index];
+        }
+
+        public int this[int i]
+        {
+            get
+            {
+                if (i <numeros.Length)
+                {
+                    return numeros[i];
+
+                }
+
+                throw new ArgumentOutOfRangeException(nameof(i),"Índice fuera del rango");
+            }
+        }
+    }
+}
